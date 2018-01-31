723ccc089933ae51533b1a2f6f4d1c3734421aa5
Doshmajhan <cpc1007@rit.edu>
2018-01-30 21:31:34 -0500

    changing slack usernmae and icon

--- app/config.py
+++ app/config.py
@@ -17,10 +17,10 @@ AUTH_API_URL = "http://lilbite.org:9000"
 
 SQLALCHEMY_DATABASE_URI = 'mysql://root:youwontguess23$@localhost/ists'
 
-SLACK_URI="https://hooks.slack.com/services/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"
+SLACK_URI = "https://hooks.slack.com/services/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"
 CHANNEL = "#white-team-tool"
-SLACK_USERNAME = "White Team"
-ICON_EMOJI = ":robot_face:"
+SLACK_USERNAME = "White Team Store"
+ICON_EMOJI = ":money_with_wings:"
 
 LOG_CONFIG = {
     'version': 1,
